0c2283faf1f70ac5b4fdd433ee166627b85d5b3c
DennisYu <973971990@qq.com>
2018-09-26 23:09:28 +0800

    添加了一些新功能

--- 0920/task3/task3/CoordinateSystem.cpp
+++ 0920/task3/task3/CoordinateSystem.cpp
@@ -13,8 +13,20 @@ CoordinateSystem::~CoordinateSystem()
 
 std::ostream & operator<<(std::ostream & out, const point & A)
 {
-
+	out << "(" << A.x << ", " << A.y << ")";
 	return out;
+}
+
+std::istream & operator>>(std::istream & in, point & A)
+{
+	using namespace std;
+	string buff;
+	getline(in, buff);
+	string _X = buff.substr(buff.find('(') + 1, buff.find(',') - buff.find('(') - 1);
+	string _Y = buff.substr(buff.find(',') + 1, buff.find(')') - buff.find(',') - 1);
+	A.x = atof(_X.c_str());
+	A.y = atof(_Y.c_str());
+	return in;
 }
 
 double delta_x(const point & A, const point & B)
@@ -32,6 +44,11 @@ point operator-(const point & A, const point & B)
 	return point({ -delta_x(A,B), -delta_y(A,B) });
 }
 
+point operator-(const point & A)
+{
+	return -1.0 * A;
+}
+
 point operator+(const point & A, const point & B)
 {
 	return point({ A.x + B.x, A.y + B.y });
@@ -39,7 +56,7 @@ point operator+(const point & A, const point & B)
 
 double abs(const point & A)
 {
-	return A.x*A.x + A.y*A.y;
+	return sqrt(A.x*A.x + A.y*A.y);
 }
 
 double slope(const point & A, const point & B)
@@ -62,14 +79,55 @@ double operator*(const point & A, const point & B)
 
 bool operator==(const point & A, const point & B)
 {
-	if (A.x == B.x && A.y == B.y)
+	if (abs(A.x - B.x) <= 5 * DBL_MIN && abs(A.y - B.y) <= 5 * DBL_MIN)
 		return true;
 	return false;
 }
 
-bool operator&&(const point & A, const point B)
+bool operator&&(const point & A, const point & B)
 {
+	double x = A * B,
+		y = abs(A) * abs(B);
 	if (A*B == abs(A)*abs(B))
 		return true;
 	return false;
+}
+
+bool operator||(const point & A, const point & B)
+{
+	if ((A && B) || ((-A) && B))
+		return true;
+	return false;
+}
+
+point operator*(const double & x, const point & A)
+{
+	return point({ x*A.x, x*A.y });
+}
+
+int DEBUG()
+{
+	using namespace std;
+	point A, B;
+	
+	cout << "Input 2 points: ";
+	cin >> A >> B;
+	
+	//A = { 1.234, 8.987 };
+	//B = 1.67807 * (-A);
+	cout << "Got point: \n";
+	cout << "A" << A << endl;
+	cout << "B" << B << endl;
+	cout << "delta(x) = " << delta_x(A, B) << endl;
+	cout << "delta(y) = " << delta_y(A, B) << endl;
+	cout << "OA - OB = " << A - B << endl;
+	cout << "OA + OB = " << A + B << endl;
+	cout << "|OA| = " << abs(A) << endl;
+	cout << "|OB| = " << abs(B) << endl;
+	cout << "|AB| = " << abs(A - B) << endl;
+	cout << "k(AB) = " << slope(A, B) << endl;
+	cout << "OA * OB = " << A * B << endl;
+	cout << "para " << (A || B) << endl;
+
+	return 0;
 }
--- 0920/task3/task3/CoordinateSystem.h
+++ 0920/task3/task3/CoordinateSystem.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+#include <string>
 
 struct point
 {
@@ -8,9 +9,14 @@ struct point
 	double y;
 };
 
-//using namespace std;
+//define a origin
+point O = { 0,0 };
+
 //输出一个点
 std::ostream & operator<<(std::ostream & out, const point & A);
+
+//输入一个点
+std::istream & operator>>(std::istream & in, point & A);
 
 //返回横坐标差值, 后减去前
 double delta_x(const point & A, const point & B);
@@ -21,6 +27,9 @@ double delta_y(const point & A, const point & B);
 //返回这两个向量的差向量
 //向量和点的表示方式一样
 point operator-(const point & A, const point & B);
+
+//返回相反向量
+point operator-(const point & A);
 
 //返回这两个向量的和向量
 point operator+(const point & A, const point & B);
@@ -39,7 +48,13 @@ double operator*(const point & A, const point & B);
 bool operator==(const point & A, const point & B);
 
 //判断向量同向
-bool operator&&(const point & A, const point B);
+bool operator&&(const point & A, const point & B);
+
+//判断向量共线
+bool operator||(const point & A, const point & B);
+
+//数乘向量
+point operator*(const double & x, const point & A);
 
 class CoordinateSystem
 {
@@ -48,3 +63,4 @@ public:
 	~CoordinateSystem();
 };
 
+int DEBUG();
