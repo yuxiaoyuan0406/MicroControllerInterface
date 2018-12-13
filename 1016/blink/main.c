dfa1561520c48b94c40c242235109a8dcf8d98fe
DennisYu <973971990@qq.com>
2018-12-13 17:27:03 +0800

    tested something, not working

--- 1016/blink/main.c
+++ 1016/blink/main.c
@@ -1,11 +1,14 @@
 #include <STC12C2052AD.H>
 //#include <reg52.h>
 
-sbit led = P2 ^ 0;
-
-void delay(unsigned int nCount)
+#define GPIO_P2		* (unsigned char *)(0xA0)
+	
+typedef unsigned int uint16_t;
+typedef unsigned char uint8_t;
+	
+void delay_ms(uint16_t nCount)
 {
-	unsigned int i;
+	uint16_t i;
 	for(; nCount; nCount--)
 		for(i = 0; i < 123; i++)
 			;
@@ -13,13 +16,34 @@ void delay(unsigned int nCount)
 
 int main()
 {
-	led = 1;
 	while(1)
 	{
-		led = ~led;
-		delay(1);
+		GPIO_P2 = 0xf;
+		delay_ms(250);
+		GPIO_P2 = 0x0;
+		delay_ms(250);
 	}
 }
+
+//sbit led = P2 ^ 0;
+
+//void delay(unsigned int nCount)
+//{
+//	unsigned int i;
+//	for(; nCount; nCount--)
+//		for(i = 0; i < 123; i++)
+//			;
+//}
+
+//int main()
+//{
+//	led = 1;
+//	while(1)
+//	{
+//		led = ~led;
+//		delay(1);
+//	}
+//}
 
 //sbit LED = P1^7;
 //sbit KEY = P3^7;
