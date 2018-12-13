d8fcb7fb14b3f2710293c5d048ac961df40ea164
DennisYu <973971990@qq.com>
2018-12-13 17:41:13 +0800

    add new project period measure

---
+++ 1204/period_measure/USER/system.h
@@ -0,0 +1,16 @@
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+//#define PERIOD_MODE
+
+#include <STC89C5xRC.H>
+#include <absacc.h>
+
+typedef unsigned char	uint8_t;
+typedef unsigned int	uint16_t;
+typedef unsigned long	uint32_t;
+
+// soft delay
+void delay_ms(uint16_t nCount);
+
+#endif	// !__SYSTEM_H
